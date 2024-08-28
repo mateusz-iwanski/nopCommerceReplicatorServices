e04dbd6467a1f778c1a6995c79db0c357aa0e4b4
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-28 15:30:37 +0200

    Init init DeserializeResponseAttribute
    Used to deserialize the response from the Web Api server

---
+++ Helpers/DeserializeResponseAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// <c>DeserializeResponseAttribute</c> is used to deserialize the response from the Web Api server
+/// </summary>
+/// <remarks>
+/// It works with the CheckAndDeserializeResponseAsync.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class DeserializeResponseAttribute : Attribute
+{
+}
