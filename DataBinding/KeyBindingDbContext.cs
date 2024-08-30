b3810ff263cd1c47baefd62003e7e15174e0b63d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-30 15:01:36 +0200

    Create a DB context for the KeyBinding

---
+++ DataBinding/KeyBindingDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.DataBinding
+{
+    internal class KeyBindingDbContext : DbContext
+    {
+        private string _connectionString { get; set; }  
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("settings.json", optional: false, reloadOnChange: true)
+                .Build();
+
+            _connectionString = configuration.GetSection("DbConnectionStrings").GetValue<string>("KeyBinding");
+
+            optionsBuilder.UseSqlServer(_connectionString);
+        }
+    }
+}
