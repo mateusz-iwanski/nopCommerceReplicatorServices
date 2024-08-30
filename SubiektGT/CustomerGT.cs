163d57828dae803ad459d64c777e299ab5749f0d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-30 15:00:02 +0200

    Refactor Get function in CustomerGT

--- SubiektGT/CustomerGT.cs
+++ SubiektGT/CustomerGT.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using NLog.Web;
 using nopCommerceReplicatorServices.Services;
 using nopCommerceWebApiClient;
@@ -19,6 +21,7 @@ using System.Numerics;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace nopCommerceReplicatorServices.SubiektGT
 {
@@ -36,8 +39,15 @@ namespace nopCommerceReplicatorServices.SubiektGT
             return;
         }
 
-        public CustomerCreatePLDto? GetCustomer(int customerId)
+        /// <summary>
+        /// Gets a customer by a specified field and value.
+        /// </summary>
+        /// <param name="fieldName">The field name to query by.</param>
+        /// <param name="fieldValue">The field value to query by.</param>
+        /// <returns>A CustomerCreatePLDto object if found; otherwise, null.</returns>
+        public IEnumerable<CustomerCreatePLDto>? Get(string fieldName, object fieldValue)
         {
+            List<CustomerCreatePLDto> products = new List<CustomerCreatePLDto>();
 
             var query = @$"
                 SELECT
@@ -54,44 +64,48 @@ namespace nopCommerceReplicatorServices.SubiektGT
                     adr_Miejscowosc,
                     woj_nazwa
                 FROM kh__Kontrahent  INNER JOIN adr__Ewid  ON kh_Id = adr_IdObiektu AND adr_TypAdresu=1
-                LEFT JOIN sl_Wojewodztwo  on woj_id=adr_idwojewodztwo WHERE kH_Id = {customerId}
-            ";
+                LEFT JOIN sl_Wojewodztwo  on woj_id=adr_idwojewodztwo WHERE {fieldName} = '{fieldValue}';
+            ";            
 
             dbConnector.OpenConnection();
 
-            SqlDataReader reader = dbConnector.ExecuteQuery(query);
 
-            if (reader.Read())
+            dbConnector.ExecuteQuery(query, (reader) =>
             {
-                var product = new CustomerCreatePLDto
+                while (reader.Read())
                 {
-                    City = reader.GetString(reader.GetOrdinal("adr_Miejscowosc")),
-                    Company = string.IsNullOrEmpty(reader.GetString(reader.GetOrdinal("adr_NIP"))) ?
-                                    null :
-                                    reader.GetString(reader.GetOrdinal("adr_Miejscowosc")),
-                    County = reader.GetString(reader.GetOrdinal("woj_nazwa")),
-                    Email = reader.GetString(reader.GetOrdinal("kh_email")),
-                    FirstName = reader.GetString(reader.GetOrdinal("kh_Imie")),
-                    LastName = reader.GetString(reader.GetOrdinal("kh_Nazwisko")),
-                    Password = reader.GetString(reader.GetOrdinal("kh_email")),  // default password is email
-                    Phone = reader.GetString(reader.GetOrdinal("adr_Telefon")),
-                    StreetAddress = reader.GetString(reader.GetOrdinal("adr_Adres")),
-                    StreetAddress2 = null,
-                    Username = reader.GetString(reader.GetOrdinal("kh_email")),
-                    ZipPostalCode = reader.GetString(reader.GetOrdinal("adr_Kod")),
-                };
+                    if (string.IsNullOrEmpty(reader.GetString(reader.GetOrdinal("kh_email"))))
+                    {
+                        Console.WriteLine("A Subiekt GT customer must have an email address to be added to nopCommerce");
+                    }
+                    else
+                    {                        
+                        var product = new CustomerCreatePLDto
+                        {
+                            City = reader.IsDBNull("adr_Miejscowosc") ? null : reader.GetString(reader.GetOrdinal("adr_Miejscowosc")),
+                            Company = reader.IsDBNull("adr_NIP") ? null : reader.GetString(reader.GetOrdinal("adr_NIP")),
+                            County = reader.IsDBNull("woj_nazwa") ? null : reader.GetString(reader.GetOrdinal("woj_nazwa")),
+                            Email = reader.GetString(reader.GetOrdinal("kh_email")),
+                            FirstName = reader.IsDBNull("kh_Imie") ? null : reader.GetString(reader.GetOrdinal("kh_Imie")),
+                            LastName = reader.IsDBNull("kh_Nazwisko") ? null : reader.GetString(reader.GetOrdinal("kh_Nazwisko")),
+                            Password = reader.GetString(reader.GetOrdinal("kh_email")),  // default password is email
+                            Phone = reader.IsDBNull("adr_Telefon") ? null : reader.GetString(reader.GetOrdinal("adr_Telefon")),
+                            StreetAddress = reader.IsDBNull("adr_Adres") ? null : reader.GetString(reader.GetOrdinal("adr_Adres")),
+                            StreetAddress2 = null,
+                            Username = reader.GetString(reader.GetOrdinal("kh_email")), // default username is email
+                            ZipPostalCode = reader.IsDBNull("adr_Kod") ? null : reader.GetString(reader.GetOrdinal("adr_Kod")),
+                        };
 
-                dbConnector.CloseConnection();
+                        products.Add(product);
+                    }
 
-                return product;
-            }
+                }
+            });
 
             dbConnector.CloseConnection();
 
-            return null;
+            return products.Count > 0 ? products : null;
         }
-
-        
 
     }
 }
