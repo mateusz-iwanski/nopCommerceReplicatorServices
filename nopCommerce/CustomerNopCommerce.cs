e9ab8fcec29414f1abfb76ee93ae3ad2aa789716
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-30 14:56:48 +0200

    Add new service to CustomerNopCommerce

--- nopCommerce/CustomerNopCommerce.cs
+++ nopCommerce/CustomerNopCommerce.cs
@@ -2,6 +2,7 @@
 using nopCommerceWebApiClient;
 using nopCommerceWebApiClient.Interfaces.Customer;
 using nopCommerceWebApiClient.Objects.Customer;
+using Refit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,13 +20,32 @@ namespace nopCommerceReplicatorServices.nopCommerce
             _customerApi = apiServices.CustomerService;
         }
 
+        /// <summary>
+        /// The default password is email
+        /// </summary>
         [DeserializeResponse]
         public async Task<HttpResponseMessage> CreatePL(int customerId, ICustomerSourceData customerGate)
         {
-            CustomerCreatePLDto customerFromGate = customerGate.GetCustomer(customerId) ?? throw new Exception($"Customer with ID {customerId} does not exist in the source data");
-            var response = await _customerApi.CreatePLAsync(customerFromGate);
+            IEnumerable<CustomerCreatePLDto>? customerFromGate = customerGate.Get("kH_Id", customerId.ToString()) ?? throw new Exception($"Customer does not exist in the source data");
+
+            var response = await _customerApi.CreatePLAsync(customerFromGate.FirstOrDefault()); 
 
             return response;
         }
+
+        [DeserializeResponse]
+        public async Task<IEnumerable<CustomerDto>> GetAllAsync() => await _customerApi.GetAllAsync();
+
+        [DeserializeResponse]
+        public async Task<CustomerDto> GetByIdAsync(int id) => await _customerApi.GetByIdAsync(id);
+
+        [DeserializeResponse]
+        public async Task<HttpResponseMessage> ConnectToAddressAsync(Guid customerGuid, int addressId) => await _customerApi.ConnectToAddressAsync(customerGuid, addressId);
+
+        [DeserializeResponse]
+        public async Task<CustomerDto> UpdatePLAsync(CustomerPLUpdateDto updateCustomerDto) => await _customerApi.UpdatePLAsync(updateCustomerDto);
+
+        [DeserializeResponse]
+        public async Task<HttpResponseMessage> UpdatePasswordAsync(Guid customerGuid, string newPassword) => await _customerApi.UpdatePasswordAsync(customerGuid, new Password(newPassword));
     }
 }
