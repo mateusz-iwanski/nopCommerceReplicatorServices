364f16b824775754fc6a6b0771ce6cc208423246
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-08-30 14:54:27 +0200

    Create Startup service

---
+++ Startup.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using nopCommerceReplicatorServices.nopCommerce;
+using nopCommerceReplicatorServices.Services;
+using nopCommerceReplicatorServices.SubiektGT;
+using nopCommerceWebApiClient;
+using System;
+using System.Collections.Generic;
+
+namespace nopCommerceReplicatorServices
+{
+    /// <summary>
+    /// Represents a builder for configuring services.
+    /// </summary>
+    public class Startup
+    {
+        /// <summary>
+        /// Initializes a new instance of the ConfigureServices class.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Register services with keys
+            services.AddScoped<CustomerNopCommerce>();
+            services.AddScoped<CustomerGT>();
+
+            services.AddScoped<IApiConfigurationServices, ApiConfigurationServices>();
+
+            // nopCommerce services
+            services.AddScoped<Func<string, ICustomer>>(serviceProvider => key =>
+            {
+                return key switch
+                {
+                    "CustomerNopCommerce" => serviceProvider.GetService<CustomerNopCommerce>() as ICustomer,                    
+                    _ => throw new ArgumentException($"Unknown key: {key}")
+                };
+            });
+
+            // source services
+            services.AddScoped<Func<string, ICustomerSourceData>>(serviceProvider => key =>
+            {
+                return key switch
+                {
+                    "CustomerGT" => serviceProvider.GetService<CustomerGT>() as ICustomerSourceData,
+                    _ => throw new ArgumentException($"Unknown key: {key}")
+                };
+            });
+        }
+    }
+}
