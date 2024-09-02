89675912551d2b1027c4299889f806382cd14079
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-02 20:49:39 +0200

    Init Customer interface

---
+++ Actions/ICustomer.cs
@@ -0,0 +1,14 @@
+﻿using nopCommerceWebApiClient.Objects.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Services
+{
+    public interface ICustomer
+    {
+        Task<HttpResponseMessage>? CreatePL(int customerId, ICustomerSourceData customerGate, Service setService);
+    }
+}
