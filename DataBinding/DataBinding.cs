8323445335026eebeb9045dce6f01b9dc01a3ec2
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-02 20:52:13 +0200

    Init DataBinding class
    Class for pairing data between nopCommerce and external services

---
+++ DataBinding/DataBinding.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.DataBinding
+{
+    public class DataBinding
+    {
+        IServiceProvider _serviceProvider;
+
+        public DataBinding(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public void AddKeyBinding(int nopCommerceCustomerId, string serviceName, string serviceKey)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
+
+                var customerKeyBinding = new CustomerDataBinding
+                {
+                    NopCommerceId = nopCommerceCustomerId,
+                    ServiceName = serviceName,
+                    ServiceKey = serviceKey
+                };
+
+                dbContext.Customers.Add(customerKeyBinding);
+                dbContext.SaveChanges();
+            }
+        }
+
+        public CustomerDataBinding? GetKeyBinding(Service serviceName, string serviceKey)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
+
+                return dbContext.Customers.FirstOrDefault(x => x.ServiceName == serviceName.ToString() && x.ServiceKey == serviceKey);
+            }
+        }
+    }
+}
