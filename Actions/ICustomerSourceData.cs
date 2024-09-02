b51be248c5cdf6cec9810f746502383e8c93ffc8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-02 20:50:04 +0200

    Init CustomerSourceData interface
    Interface for external services

---
+++ Actions/ICustomerSourceData.cs
@@ -0,0 +1,14 @@
+﻿using nopCommerceWebApiClient.Objects.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Services
+{
+    public interface ICustomerSourceData
+    {
+        IEnumerable<CustomerCreatePLDto>? Get(string fieldName, object fieldValue);
+    }
+}
