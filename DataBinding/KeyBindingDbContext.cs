ea99063b48107d73eb8f7febb85616bfe1399968
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-03 12:20:49 +0200

    Read the configuration by the DI service provider in the KeyBindingDbContext

--- DataBinding/KeyBindingDbContext.cs
+++ DataBinding/KeyBindingDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,16 +11,20 @@ namespace nopCommerceReplicatorServices.DataBinding
 {
     internal class KeyBindingDbContext : DbContext
     {
+        private IServiceProvider _serviceProvider { get; set; }
+
         private string _connectionString { get; set; }  
 
         public DbSet<CustomerDataBinding> Customers { get; set; }
 
+        public KeyBindingDbContext(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("settings.json", optional: false, reloadOnChange: true)
-                .Build();
+            var configuration = _serviceProvider.GetRequiredService<IConfiguration>();
 
             _connectionString = configuration.GetSection("DbConnectionStrings").GetValue<string>("KeyBinding");
 
