9fa659644daed28786eadd488a7e52e9ecab0d98
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-05 20:00:29 +0200

    Init ProductNopCommerce

---
+++ nopCommerce/ProductNopCommerce.cs
@@ -0,0 +1,31 @@
+﻿using nopCommerceReplicatorServices.Actions;
+using nopCommerceWebApiClient;
+using nopCommerceWebApiClient.Interfaces.Customer;
+using nopCommerceWebApiClient.Interfaces.Product;
+using nopCommerceWebApiClient.Objects.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    internal class ProductNopCommerce : IProduct
+    {
+        private IProductService _productApi { get; set; }
+        private readonly IServiceProvider _serviceProvider;
+
+        public ProductNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
+        {
+            _productApi = apiServices.ProductService;
+            _serviceProvider = serviceProvider;
+        }
+
+        [DeserializeResponse]
+        public async Task<ProductDto?> GetProductById(int productId)
+        {
+            return await _productApi.GetByIdAsync(productId);
+        }
+    }
+}
