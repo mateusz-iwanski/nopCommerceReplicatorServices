b59f42c2af650fbe3e991eb75153fc9b9e810118
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-06 18:14:56 +0200

    Init ITax interface

---
+++ Actions/ITax.cs
@@ -0,0 +1,14 @@
+﻿using nopCommerceReplicatorServices.SubiektGT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Actions
+{
+    public interface ITax
+    {
+        Task<int> GetCategoryByPercentageAsync(VatLevel percentage);
+    }
+}
