c01cf1518cf718a2fac0f3e4a4e1497a798e2127
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-06 18:17:30 +0200

    Init PriceLevelGT

---
+++ SubiektGT/PriceLevelGT.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.SubiektGT
+{
+    /// <summary>
+    /// SubiektGT default has 2 price levels, index price and retail price. 
+    /// You can add more price levels in SubiektGT.
+    /// </summary>
+    public enum PriceLevelGT
+    {
+        tc_CenaNetto0, // index price
+        tc_CenaNetto1, // retail price
+        tc_CenaNetto2, // wholesale price
+        tc_CenaNetto3, // special price
+    }
+}
