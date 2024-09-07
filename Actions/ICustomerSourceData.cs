1742b4b7a781dcb1f87dd15688cbbe5d2631351e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-07 21:12:08 +0200

    Make a summary for ICustomerSourceData

--- Actions/ICustomerSourceData.cs
+++ Actions/ICustomerSourceData.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace nopCommerceReplicatorServices.Services
 {
+    /// <summary>
+    /// This interface is used to implement customer actions for external services with source data (e.g. CustomerGt, CustomerDjango, ...).
+    /// </summary>
     public interface ICustomerSourceData
     {
         IEnumerable<CustomerDto>? Get(string fieldName, object fieldValue);
