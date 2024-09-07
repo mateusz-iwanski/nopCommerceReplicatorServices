5b0f49faf84f7b9a033e4c5cd23c957418b05e93
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-07 21:16:07 +0200

    Rename the file and class DeserializeWebApiNopCommerceResponseAttribute

---
+++ Helpers/DeserializeWebApiNopCommerceResponseAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>
+/// <c>DeserializeWebApiNopCommerceResponseAttribute</c> is used to deserialize the response from the Web Api server
+/// </summary>
+/// <remarks>
+/// It works with the DeserializeWebApiNopCommerceResponseAsync.
+/// </remarks>
+[AttributeUsage(AttributeTargets.Method, Inherited = true)]
+public class DeserializeWebApiNopCommerceResponseAttribute : Attribute
+{
+}
