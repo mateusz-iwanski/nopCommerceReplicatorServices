42427bf6012e3c9f5fcb128bd59eec73f07fe176
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-07 21:15:15 +0200

    Rename the method declaration in ITax

--- Actions/ITax.cs
+++ Actions/ITax.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace nopCommerceReplicatorServices.Actions
 {
     public interface ITax
-    {
-        Task<int> GetCategoryByPercentageAsync(VatLevel percentage);
+    {        
+        Task<int> GetCategoryByNameAsync(VatLevel percentage);
     }
 }
