c1d2b65b943601c8de613909cfde64192a29bbba
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-09 21:12:57 +0200

    Rename method to CreateProductWithMinimalDataAsync
    Updated method name from CreateWithMinimalData to CreateProductWithMinimalDataAsync in Program.cs. This change affects the method call on productNopCommerceService and the string parameter in DeserializeWebApiNopCommerceResponseAsync. The update ensures consistency and reflects asynchronous processing.

--- Program.cs
+++ Program.cs
@@ -210,7 +210,7 @@ internal partial class Program
                     IProduct productNopCommerceService = scope.ServiceProvider.GetRequiredService<Func<string, IProduct>>()("ProductNopCommerce");
                     IProductSourceData productDataSourceService = scope.ServiceProvider.GetRequiredService<Func<string, IProductSourceData>>()(productService);
 
-                    IEnumerable<HttpResponseMessage>? response = await productNopCommerceService.CreateWithMinimalData(repProductIdOption, productDataSourceService, Enum.Parse<Service>(serviceToReplicate));
+                    IEnumerable<HttpResponseMessage>? response = await productNopCommerceService.CreateProductWithMinimalDataAsync(repProductIdOption, productDataSourceService, Enum.Parse<Service>(serviceToReplicate));
 
                     if (response == null)
                     {
@@ -223,7 +223,7 @@ internal partial class Program
                         Console.WriteLine($"Replicate product with ID: {repProductIdOption} --- Status code: {(int)responseList[0].StatusCode} ({responseList[0].StatusCode}).");
                         Console.WriteLine($"Update product Gtin with ID: {repProductIdOption} --- Status code: {(int)responseList[1].StatusCode} ({responseList[1].StatusCode}).");
 
-                        if (showDetailsOption) await AttributeHelper.DeserializeWebApiNopCommerceResponseAsync<ProductNopCommerce>("CreateWithMinimalData", responseList);
+                        if (showDetailsOption) await AttributeHelper.DeserializeWebApiNopCommerceResponseAsync<ProductNopCommerce>("CreateProductWithMinimalDataAsync", responseList);
                     }
                 }
             }
