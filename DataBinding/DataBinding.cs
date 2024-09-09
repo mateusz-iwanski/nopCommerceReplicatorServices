af4a74d94d96ba82660c92c75c71b63b1d570563
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-09 21:11:04 +0200

    Refactor DataBinding class and update key bindings
    - Added XML documentation to `DataBinding` class and methods.
    - Renamed `AddKeyBinding` to `BindKey` for clarity.
    - Changed `customerKeyBinding` type to `DataBindingEntity`.
    - Updated DbSet property name to `DataBinding` in `KeyBindingDbContext`.
    - Renamed `GetKeyBinding` method and updated its return type.
    - Modified LINQ query in `GetKeyBinding` to use `DataBinding` DbSet.

--- DataBinding/DataBinding.cs
+++ DataBinding/DataBinding.cs
@@ -7,6 +7,15 @@ using System.Threading.Tasks;
 
 namespace nopCommerceReplicatorServices.DataBinding
 {
+    /// <summary>
+    /// Bind data between nopCommerce and external services.
+    /// Data are stored in the database.
+    /// </summary>
+    /// <remarks>
+    /// For example, associate a customer ID from nopCommerce with a customer ID from an external service,
+    /// so that we know which nopCommerce customer is which customer on the external site or 
+    /// whether the client has already been replicated from an external service.
+    /// </remarks>
     public class DataBinding
     {
         IServiceProvider _serviceProvider;
@@ -16,13 +25,20 @@ namespace nopCommerceReplicatorServices.DataBinding
             _serviceProvider = serviceProvider;
         }
 
-        public void AddKeyBinding(int nopCommerceCustomerId, string serviceName, string serviceKey, string serviceValue)
+        /// <summary>
+        /// Bind a ID from nopCommerce with a serviceName (ID, symbol etc.) from an external service.
+        /// </summary>
+        /// <param name="nopCommerceCustomerId">The customer ID from nopCommerce</param>
+        /// <param name="serviceName">The key name of the external service (should be the same as ICustomer.ServiceKeyName)</param>  
+        /// <param name="serviceKey">The key name of the external service. Identifier, symbol... any unique you want to use)</param>
+        /// <param name="serviceValue">The value of the external service. For example - if ID - "1","2","3" ... If symbol - "u1" "sas3" ... </param>
+        public void BindKey(int nopCommerceCustomerId, string serviceName, string serviceKey, string serviceValue)
         {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
 
-                var customerKeyBinding = new CustomerDataBinding
+                var customerKeyBinding = new DataBindingEntity
                 {
                     NopCommerceId = nopCommerceCustomerId,
                     ServiceName = serviceName,
@@ -30,18 +46,25 @@ namespace nopCommerceReplicatorServices.DataBinding
                     ServiceValue = serviceValue
                 };
 
-                dbContext.Customers.Add(customerKeyBinding);
+                dbContext.DataBinding.Add(customerKeyBinding);
                 dbContext.SaveChanges();
             }
         }
 
-        public CustomerDataBinding? GetKeyBinding(Service serviceName, string serviceKey, string serviceValue)
+        /// <summary>
+        /// Get client binding information.
+        /// If it isn't found, we know it hasn't been replicated yet.
+        /// </summary>
+        /// <param name="serviceName">The key name of the external service (should be the same as ICustomer.ServiceKeyName)</param>  
+        /// <param name="serviceKey">The key name of the external service. Identifier, symbol... any unique you used)</param>
+        /// <param name="serviceValue">The value of the external service. For example - if used ID - "1","2","3" ... If used symbol - "u1" "sas3" ... </param>
+        public DataBindingEntity? GetKeyBinding(Service serviceName, string serviceKey, string serviceValue)
         {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
 
-                return dbContext.Customers.FirstOrDefault(x =>
+                return dbContext.DataBinding.FirstOrDefault(x =>
                     x.ServiceName == serviceName.ToString() &&
                     x.ServiceKey == serviceKey &&
                     x.ServiceValue == serviceValue
