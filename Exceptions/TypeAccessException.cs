19019eb88ada4ed2ab47356f768d546e685ffa6a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-10 22:09:43 +0200

    Add TypeAccessException class to handle type access errors
    Introduced a new file `TypeAccessException.cs` with the following:
    - Added necessary `using` directives.
    - Created `nopCommerceReplicatorServices.Exceptions` namespace.
    - Defined `TypeAccessException` class inheriting from `CustomException`.
    - Implemented constructors for message and inner exception handling.

---
+++ Exceptions/TypeAccessException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Exceptions
+{
+    public class TypeAccessException : CustomException
+    {
+        public TypeAccessException(string message) : base(message)
+        {
+        }
+
+        public TypeAccessException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+    }
+}
