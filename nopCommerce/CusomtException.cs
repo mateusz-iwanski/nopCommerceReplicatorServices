db3f2fd05f9c515f61d4a085d73c60f620676270
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-10 22:10:00 +0200

    Add CusomtException class in new namespace
    Introduced a new namespace `nopCommerceReplicatorServices.nopCommerce` and added a `[Serializable]` internal class `CusomtException` with three constructors: a parameterless constructor, a constructor with a nullable string `message`, and a constructor with a nullable string `message` and a nullable `Exception` object `innerException`. This class is designed for handling exceptions specific to the nopCommerce replicator services.

---
+++ nopCommerce/CusomtException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    [Serializable]
+    internal class CusomtException : Exception
+    {
+        public CusomtException()
+        {
+        }
+
+        public CusomtException(string? message) : base(message)
+        {
+        }
+
+        public CusomtException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
