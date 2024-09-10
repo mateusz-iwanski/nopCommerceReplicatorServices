bb304e3e82ba784298ef1202853221d1cec985cc
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-10 22:08:57 +0200

    Add ArgumentException class to custom exceptions
    Introduce a new ArgumentException class in the nopCommerceReplicatorServices.Exceptions namespace. This class inherits from CustomException and includes two constructors: one for a message string and another for a message string and an inner exception. Added necessary using directives.

---
+++ Exceptions/ArgumentException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Exceptions
+{
+    public class ArgumentException : CustomException
+    {
+        public ArgumentException(string message) : base(message)
+        {
+        }
+
+        public ArgumentException(string message, Exception inner) : base(message, inner)
+        {
+        }
+    }
+}
