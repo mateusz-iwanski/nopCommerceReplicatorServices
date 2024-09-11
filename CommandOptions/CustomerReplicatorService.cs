6614f6a923cca974ef2ed88eac983325afe7d4eb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-11 16:09:42 +0200

    Add CustomerReplicatorService for customer data replication
    Introduced a new class `CustomerReplicatorService` in the
    `nopCommerceReplicatorServices.CommandOptions` namespace. This class
    includes an asynchronous method `ReplicateCustomerAsync` to replicate
    customer data. The method handles various parameters, performs
    validations, creates service scopes, and interacts with customer
    services to replicate data. It also provides detailed output based on
    the `showDetailsOption` parameter.

---
+++ CommandOptions/CustomerReplicatorService.cs
@@ -0,0 +1,50 @@
+﻿using Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using nopCommerceReplicatorServices.nopCommerce;
+using nopCommerceReplicatorServices.Services;
+using nopCommerceWebApiClient.Interfaces.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace nopCommerceReplicatorServices.CommandOptions
+{
+    public class CustomerReplicatorService
+    {
+        public async Task ReplicateCustomerAsync(string serviceToReplicate, IServiceProvider serviceProvider, IConfiguration configuration, int repCustomerId, bool showDetailsOption)
+        {
+            if (string.IsNullOrEmpty(serviceToReplicate))
+            {
+                Console.WriteLine("Invalid or missing service to replicate. Use replicate_service to set up a service for replication.");
+                return;
+            }
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                // get customer service which is marked for replication
+                var customerService = configuration.GetSection("Service").GetSection(serviceToReplicate).GetValue<string>("Customer");
+
+                ICustomer customerNopCommerceService = scope.ServiceProvider.GetRequiredService<Func<string, ICustomer>>()("CustomerNopCommerce");
+                ICustomerSourceData customerDataSourceService = scope.ServiceProvider.GetRequiredService<Func<string, ICustomerSourceData>>()(customerService);
+
+                HttpResponseMessage? response = await customerNopCommerceService.CreatePLAsync(repCustomerId, customerDataSourceService, Enum.Parse<Service>(serviceToReplicate));
+
+                if (response == null)
+                {
+                    Console.WriteLine($"Adding failed. Customer with ID: {repCustomerId} already exists in the database.");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Replicate customer with ID: {repCustomerId} --- Status code: {(int)response.StatusCode} ({response.StatusCode}).");
+
+                    if (showDetailsOption) await AttributeHelper.DeserializeWebApiNopCommerceResponseAsync<CustomerNopCommerce>("CreatePLAsync", response);
+                }
+            }
+        }
+    }
+}
