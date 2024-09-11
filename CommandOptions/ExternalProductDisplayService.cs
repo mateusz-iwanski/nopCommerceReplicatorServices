e8fa9b810cacbb66c0d072f5be016ddd87063131
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-11 16:10:08 +0200

    Add ExternalProductDisplayService with ShowProductAsync
    Introduce ExternalProductDisplayService in nopCommerceReplicatorServices.CommandOptions namespace. This class includes the ShowProductAsync method to display product information asynchronously. The method handles parameters for service replication, configuration, and product details, and retrieves product data using IProductSourceData. Necessary using directives for dependency injection and configuration are also included.

---
+++ CommandOptions/ExternalProductDisplayService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using nopCommerceReplicatorServices.Actions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.CommandOptions
+{
+    public class ExternalProductDisplayService
+    {
+        public async Task ShowProductAsync(string serviceToReplicate, IServiceProvider serviceProvider, IConfiguration configuration, int shProductIdOption, bool showDetailsOption)
+        {
+            if (string.IsNullOrEmpty(serviceToReplicate))
+            {
+                Console.WriteLine("Invalid or missing service to replicate. Use replicate_service to set up a service for replication.");
+                return;
+            }
+
+            var productService = configuration.GetSection("Service").GetSection(serviceToReplicate).GetValue<string>("Product");
+
+            using (var scope = serviceProvider.CreateScope())
+            {
+                IProductSourceData productDataSourceService = scope.ServiceProvider.GetRequiredService<Func<string, IProductSourceData>>()(productService);
+
+                var productDto = await productDataSourceService.GetByIdAsync(shProductIdOption);
+
+                if (productDto != null)
+                    Console.WriteLine($"Response: {productDto.ToString()}");
+            }
+        }
+
+    }
+}
