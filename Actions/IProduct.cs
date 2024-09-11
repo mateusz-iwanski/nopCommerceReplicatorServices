b6330c992b9468d0d5c1984e6528a9011c41e1e7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-09-11 16:08:59 +0200

    Refactor IProduct interface and update documentation
    Updated interface documentation to clarify usage for target data. Added `ServiceKeyName` property to `IProduct` interface. Renamed `CreateProductWithMinimalDataAsync` to `CreateMinimalProductAsync` for clarity. Enhanced XML documentation for `CreateMinimalProductAsync` method.

--- Actions/IProduct.cs
+++ Actions/IProduct.cs
@@ -4,11 +4,22 @@ using nopCommerceWebApiClient.Objects.Product;
 namespace nopCommerceReplicatorServices.Actions
 {
     /// <summary>
-    /// This interface is used to implement _source actions for target data (e.g. ProductNopCommerce).
+    /// This interface is used to implement product actions for target data (e.g. ProductNopCommerce).
     /// </summary>
     public interface IProduct
     {
+        /// <summary>
+        /// Gets the service key name for the product API.
+        /// </summary>
         string ServiceKeyName { get; }
-        Task<IEnumerable<HttpResponseMessage>>? CreateProductWithMinimalDataAsync(int customerId, IProductSourceData productGate, Service setService);
+
+        /// <summary>
+        /// Creates a product in nopCommerce from SubiektGT with minimal data.
+        /// </summary>
+        /// <param name="productId">The ID of the product.</param>
+        /// <param name="productExternal">The external product source data.</param>
+        /// <param name="setService">The chosen service.</param>
+        /// <returns>The list of HTTP response messages.</returns>
+        Task<IEnumerable<HttpResponseMessage>>? CreateMinimalProductAsync(int customerId, IProductSourceData productGate, Service setService);
     }
 }
