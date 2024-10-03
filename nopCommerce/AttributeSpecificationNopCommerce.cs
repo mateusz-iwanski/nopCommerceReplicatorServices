84e6e3494a901bec8e92eb5c820c02a242c30769
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-03 15:33:42 +0200

    Add AttributeSpecificationNopCommerce class
    Introduce internal class `AttributeSpecificationNopCommerce` in the `nopCommerceReplicatorServices.nopCommerce` namespace. This class manages specification attributes using `ISpecificationAttributeService`, `ISpecificationAttributeOptionService`, and `ISpecificationAttributeGroupService`, all injected via the constructor along with a readonly `IServiceProvider`.

---
+++ nopCommerce/AttributeSpecificationNopCommerce.cs
@@ -0,0 +1,31 @@
+﻿using nopCommerceWebApiClient.Interfaces.Product;
+using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    internal class AttributeSpecificationNopCommerce
+    {
+        private ISpecificationAttributeService _specificationAttributerApi { get; set; }
+        private ISpecificationAttributeOptionService _specificationAttributeOptionService { get; set; }
+        private ISpecificationAttributeGroupService _specificationAttributeGroupService { get; set; }
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public AttributeSpecificationNopCommerce(
+            ISpecificationAttributeService specificationAttributerApi,
+            ISpecificationAttributeOptionService specificationAttributeOptionService,
+            ISpecificationAttributeGroupService specificationAttributeGroupService,
+            IServiceProvider serviceProvider)
+        {
+            _specificationAttributerApi = specificationAttributerApi;
+            _specificationAttributeOptionService = specificationAttributeOptionService;
+            _specificationAttributeGroupService = specificationAttributeGroupService;
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
