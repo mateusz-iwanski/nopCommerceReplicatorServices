4529c4ff94e561d2994c3fc9b88783902c3e6095
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-03 15:38:14 +0200

    Add AttributeSpecificationOptionNopCommerce class
    Introduce a new internal class `AttributeSpecificationOptionNopCommerce` in the `nopCommerceReplicatorServices.nopCommerce` namespace. Added necessary using directives for `nopCommerceWebApiClient`, `nopCommerceWebApiClient.Interfaces.SpecificationAttribute`, `System`, `System.Collections.Generic`, `System.Linq`, `System.Text`, and `System.Threading.Tasks`.
    
    The class includes a private property `_specificationAttributeOptionApi` of type `ISpecificationAttributeOptionService` and a private readonly field `_serviceProvider` of type `IServiceProvider`. A constructor is defined to initialize these fields using `IApiConfigurationServices` and `IServiceProvider` parameters.

---
+++ nopCommerce/AttributeSpecificationOptionNopCommerce.cs
@@ -0,0 +1,23 @@
+﻿using nopCommerceWebApiClient;
+using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    internal class AttributeSpecificationOptionNopCommerce
+    {
+        private ISpecificationAttributeOptionService _specificationAttributeOptionApi { get; set; }
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public AttributeSpecificationOptionNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
+        {
+            _specificationAttributeOptionApi = apiServices.SpecificationAttributeOptionService;
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
