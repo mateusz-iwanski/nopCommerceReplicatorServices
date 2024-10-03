eb7b4decd9f539589bbedc74ffe15104c6e3d71c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-03 15:40:26 +0200

    Refactor AttributeSpecificationNopCommerce constructor
    Reorganized using directives for better readability.
    Removed private properties `_specificationAttributeOptionService` and `_specificationAttributeGroupService`.
    Modified constructor to accept `IApiConfigurationServices` instead of individual service parameters.
    Initialized `_specificationAttributerApi` using `SpecificationAttributeService` from `IApiConfigurationServices`.

--- nopCommerce/AttributeSpecificationNopCommerce.cs
+++ nopCommerce/AttributeSpecificationNopCommerce.cs
@@ -1,4 +1,5 @@
-﻿using nopCommerceWebApiClient.Interfaces.Product;
+﻿using nopCommerceWebApiClient;
+using nopCommerceWebApiClient.Interfaces.Product;
 using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
 using System;
 using System.Collections.Generic;
@@ -11,20 +12,12 @@ namespace nopCommerceReplicatorServices.nopCommerce
     internal class AttributeSpecificationNopCommerce
     {
         private ISpecificationAttributeService _specificationAttributerApi { get; set; }
-        private ISpecificationAttributeOptionService _specificationAttributeOptionService { get; set; }
-        private ISpecificationAttributeGroupService _specificationAttributeGroupService { get; set; }
 
         private readonly IServiceProvider _serviceProvider;
 
-        public AttributeSpecificationNopCommerce(
-            ISpecificationAttributeService specificationAttributerApi,
-            ISpecificationAttributeOptionService specificationAttributeOptionService,
-            ISpecificationAttributeGroupService specificationAttributeGroupService,
-            IServiceProvider serviceProvider)
+        public AttributeSpecificationNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
         {
-            _specificationAttributerApi = specificationAttributerApi;
-            _specificationAttributeOptionService = specificationAttributeOptionService;
-            _specificationAttributeGroupService = specificationAttributeGroupService;
+            _specificationAttributerApi = apiServices.SpecificationAttributeService;
             _serviceProvider = serviceProvider;
         }
     }
