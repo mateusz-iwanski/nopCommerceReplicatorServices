6ff1a6ab0a3febd5671cf6e04ed78dfbc3e9db72
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-03 15:40:08 +0200

    Add SpecificationAttributeGroupNopCommerce class
    Introduce `SpecificationAttributeGroupNopCommerce` class in the
    `nopCommerceReplicatorServices.nopCommerce` namespace. Add necessary
    using directives. Define the class as `internal` with a private
    property `_specificationAttributeGroupApi` and a private readonly
    field `_serviceProvider`. Implement a constructor to initialize these
    fields using `IApiConfigurationServices` and `IServiceProvider`.

---
+++ nopCommerce/SpecificationAttributeGroupNopCommerce.cs
@@ -0,0 +1,23 @@
+﻿using nopCommerceWebApiClient;
+using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    internal class SpecificationAttributeGroupNopCommerce
+    {
+        private ISpecificationAttributeGroupService _specificationAttributeGroupApi { get; set; }
+
+        private readonly IServiceProvider _serviceProvider;
+
+        public SpecificationAttributeGroupNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
+        {
+            _specificationAttributeGroupApi = apiServices.SpecificationAttributeGroupService;
+            _serviceProvider = serviceProvider;
+        }
+    }
+}
