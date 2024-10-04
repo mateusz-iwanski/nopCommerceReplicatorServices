8780ae3faf0cf487693f38db35cd679fceae2b0c
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-04 12:36:03 +0200

    Add CreateAsync method and update visibility
    - Updated `using` directives to include `nopCommerceReplicatorServices.Exceptions` and `System.Net.Http.Json`.
    - Changed `AttributeSpecificationGroupNopCommerce` class from `internal` to `public`.
    - Added XML documentation to `AttributeSpecificationGroupNopCommerce` class.
    - Implemented `CreateAsync` method in `AttributeSpecificationGroupNopCommerce` class to handle creation of specification attribute groups with error handling.

--- nopCommerce/AttributeSpecificationGroupNopCommerce.cs
+++ nopCommerce/AttributeSpecificationGroupNopCommerce.cs
@@ -1,17 +1,25 @@
-﻿using nopCommerceWebApiClient;
+﻿using nopCommerceReplicatorServices.Exceptions;
+using nopCommerceWebApiClient;
 using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
 using nopCommerceWebApiClient.Objects.SpecificationAttribute;
 using nopCommerceWebApiClient.Objects.SpecyficationAttributeGroup;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Serialization;
 
 namespace nopCommerceReplicatorServices.nopCommerce
 {
-    internal class AttributeSpecificationGroupNopCommerce
+    /// <summary>
+    /// Group for attribute specification
+    /// 
+    /// For example: Product, Accessories, Board etc
+    /// </summary>
+    public class AttributeSpecificationGroupNopCommerce
     {
         private ISpecificationAttributeGroupService _specificationAttributeGroupApi { get; set; }
 
@@ -21,6 +29,35 @@ namespace nopCommerceReplicatorServices.nopCommerce
         {
             _specificationAttributeGroupApi = apiServices.SpecificationAttributeGroupService;
             _serviceProvider = serviceProvider;
+        }
+
+        /// <summary>
+        /// Create a new spec attribute group. If there is such an option, just return it.
+        /// </summary>
+        /// <param name="attributeSpecificationOptionNopCommerce">SpecificationAttributeOptionCreateDto</param>
+        /// <returns>SpecificationAttributeOptionDto or throw CustomException</returns>
+        [DeserializeWebApiNopCommerceResponse]
+        public async Task<SpecificationAttributeGroupDto> CreateAsync(SpecificationAttributeGroupCreateDto attributeSpecificationGroupNopCommerce)
+        {
+            try
+            {
+                // if exists return
+                var existing = await GetByNameAsync(attributeSpecificationGroupNopCommerce.Name);
+                if (existing != null) return existing;
+
+                // if not exists add new
+                var apiResponse = await _specificationAttributeGroupApi.CreateAsync(attributeSpecificationGroupNopCommerce);
+                if (apiResponse.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new CustomException($"Failed to create a new specification attribute group. {apiResponse.ReasonPhrase}");
+                }
+
+                return await apiResponse.Content.ReadFromJsonAsync<SpecificationAttributeGroupDto>();
+            }
+            catch (Exception ex)
+            {
+                throw new CustomException($"Failed to create a new specification attribute group. {ex.Message}");
+            }
         }
 
         /// <summary>
