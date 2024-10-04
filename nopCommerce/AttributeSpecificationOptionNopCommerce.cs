8fbddd21b5d1513c9bc442b74073b26f8670c67b
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-04 12:06:46 +0200

    Add GetByNameAsync method to AttributeSpecificationOption
    Updated using directives in AttributeSpecificationOptionNopCommerce.cs to include necessary namespaces. Added a new asynchronous method, GetByNameAsync, to retrieve a specification attribute option by its name. This method is decorated with DeserializeWebApiNopCommerceResponse and returns a SpecificationAttributeOptionDto or null.

--- nopCommerce/AttributeSpecificationOptionNopCommerce.cs
+++ nopCommerce/AttributeSpecificationOptionNopCommerce.cs
@@ -1,8 +1,12 @@
-﻿using nopCommerceWebApiClient;
+﻿using nopCommerceReplicatorServices.Exceptions;
+using nopCommerceWebApiClient;
 using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecificationAttribute;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +22,19 @@ namespace nopCommerceReplicatorServices.nopCommerce
         {
             _specificationAttributeOptionApi = apiServices.SpecificationAttributeOptionService;
             _serviceProvider = serviceProvider;
+        }
+
+        /// <summary>
+        /// Get a spec attribute option by name
+        /// </summary>
+        /// <param name="name">name of attribute option</param>
+        /// <returns>If exists SpecificationAttributeOptionDto, null if not</returns>
+        [DeserializeWebApiNopCommerceResponse]
+        public async Task<SpecificationAttributeOptionDto>? GetByNameAsync(string name)
+        {
+            var allSpecAttrOption =  await _specificationAttributeOptionApi.GetAllAsync();
+            
+            return allSpecAttrOption.FirstOrDefault(x => x.Name == name);
         }
     }
 }
