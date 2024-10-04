ea67ea2562df5fdbe87f2c8605ce84dc7ec39f5e
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-04 12:59:45 +0200

    Enhance AttributeSpecificationNopCommerce class
    Added using directives for necessary namespaces. Introduced XML documentation for the AttributeSpecificationNopCommerce class. Added GetByNameAsync method to fetch specification attributes by name, improving functionality and documentation.

--- nopCommerce/AttributeSpecificationNopCommerce.cs
+++ nopCommerce/AttributeSpecificationNopCommerce.cs
@@ -1,14 +1,28 @@
-﻿using nopCommerceWebApiClient;
+﻿using Microsoft.Extensions.DependencyInjection;
+using nopCommerceReplicatorServices.Exceptions;
+using nopCommerceWebApiClient;
 using nopCommerceWebApiClient.Interfaces.Product;
 using nopCommerceWebApiClient.Interfaces.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecyficationAttribute;
+using nopCommerceWebApiClient.Objects.SpecyficationAttributeGroup;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace nopCommerceReplicatorServices.nopCommerce
 {
+    /// <summary>
+    /// Attribute specification object. Consists of:
+    /// AttributeSpecificationOption
+    /// AttributeSpecificationGroup
+    /// 
+    /// For example: Product, Accessories, Board etc
+    /// </summary>
     internal class AttributeSpecificationNopCommerce
     {
         private ISpecificationAttributeService _specificationAttributerApi { get; set; }
@@ -20,5 +34,19 @@ namespace nopCommerceReplicatorServices.nopCommerce
             _specificationAttributerApi = apiServices.SpecificationAttributeService;
             _serviceProvider = serviceProvider;
         }
+        
+        /// <summary>
+        /// Get a specification attribute
+        /// </summary>
+        /// <param name="name">name of attribute</param>
+        /// <returns>If exists SpecificationAttributeDto, null if not</returns>
+        [DeserializeWebApiNopCommerceResponse]
+        public async Task<SpecificationAttributeDto>? GetByNameAsync(string name)
+        {
+            var allSpecAttrGroup = await _specificationAttributerApi.GetAllAsync();
+
+            return allSpecAttrGroup.FirstOrDefault(x => x.Name == name);
+        }
+
     }
 }
