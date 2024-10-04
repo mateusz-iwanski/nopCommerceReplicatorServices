5c8ec19e4cadc36a8942ca0def301cb0926c701d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-04 11:17:48 +0200

    Rename class and add methods for future functionality
    Renamed `SpecificationAttributeGroupNopCommerce` to `AttributeSpecificationGroupNopCommerce` to better reflect its purpose. Added `using System.Xml.Serialization` directive. Introduced `CreateSpecificationAttributeGroupAsync` and `UpdateSpecificationAttributeGroupAsync` methods, which currently throw `NotImplementedException`.

--- nopCommerce/AttributeSpecificationGroupNopCommerce.cs
+++ nopCommerce/AttributeSpecificationGroupNopCommerce.cs
@@ -5,19 +5,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace nopCommerceReplicatorServices.nopCommerce
 {
-    internal class SpecificationAttributeGroupNopCommerce
+    internal class AttributeSpecificationGroupNopCommerce
     {
         private ISpecificationAttributeGroupService _specificationAttributeGroupApi { get; set; }
 
         private readonly IServiceProvider _serviceProvider;
 
-        public SpecificationAttributeGroupNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
+        public AttributeSpecificationGroupNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
         {
             _specificationAttributeGroupApi = apiServices.SpecificationAttributeGroupService;
             _serviceProvider = serviceProvider;
+        }
+
+        public Task CreateSpecificationAttributeGroupAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateSpecificationAttributeGroupAsync()
+        {
+            throw new NotImplementedException();
         }
     }
 }
