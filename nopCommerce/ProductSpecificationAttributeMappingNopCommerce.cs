fac7fff809455b972c2e2736070146b6c5d6e171
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-04 14:13:10 +0200

    Add ProductSpecificationAttributeMappingNopCommerce class
    Introduced a new class `ProductSpecificationAttributeMappingNopCommerce` in the `nopCommerceReplicatorServices.nopCommerce` namespace. This class connects attribute specifications with products and initializes necessary services via its constructor. Added relevant using directives to support the new functionality.

---
+++ nopCommerce/ProductSpecificationAttributeMappingNopCommerce.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Identity.Client;
+using nopCommerceReplicatorServices.Exceptions;
+using nopCommerceWebApiClient;
+using nopCommerceWebApiClient.Interfaces.Product;
+using nopCommerceWebApiClient.Objects.Product;
+using nopCommerceWebApiClient.Objects.ProductSpecificationAttributeMapping;
+using nopCommerceWebApiClient.Objects.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecyficationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    /// <summary>
+    /// Connect attribute specyfication with product
+    /// </summary>
+    public class ProductSpecificationAttributeMappingNopCommerce
+    {
+        private readonly IServiceProvider _serviceProvider;
+        private readonly IProductSpecificationAttributeMappingService _productSpecificationAttributeMappingService;
+
+        public ProductSpecificationAttributeMappingNopCommerce(IApiConfigurationServices apiServices, IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+            _productSpecificationAttributeMappingService = apiServices.ProductSpecificationAttributeMappingService;
+        }
+
+       
+    }
+}
