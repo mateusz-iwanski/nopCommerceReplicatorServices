3de67744b24edcf6cc78c690b6fdbcad2a3181c0
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-08 15:05:45 +0200

    Add AttributeSpecificationMapperDto for attribute mapping
    Introduced a new file `AttributeSpecificationMapperDto.cs` in the `nopCommerceReplicatorServices.nopCommerce` namespace. This file defines a record type `AttributeSpecificationMapperDto` with parameters `groupName`, `value`, and `optionName` to map attribute specifications from an external source to a product. Included necessary `using` directives.

---
+++ nopCommerce/AttributeSpecificationMapperDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    /// <summary>
+    /// Object is used to map attribute specification (from external source) to product
+    /// </summary>
+    /// <param name="groupName">Product, Accessories, Board etc.</param>        
+    /// <param name="value">Color, Opening angle, etc.</param>
+    /// <param name="optionName">Red, Black, Left corner etc.</param>
+    public record AttributeSpecificationMapperDto(string groupName, string value, string optionName);
+}
