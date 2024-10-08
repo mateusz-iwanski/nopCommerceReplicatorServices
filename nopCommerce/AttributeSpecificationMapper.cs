f3c2b4da14b86355a26293d790d3e2b0152ab8ce
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-08 14:51:06 +0200

    Add AttributeSpecificationMapper class for attribute mapping
    Added a new class `AttributeSpecificationMapper` in the
    `nopCommerceReplicatorServices.nopCommerce` namespace. This class
    maps attribute specifications to products using various services
    and DTOs. It includes private fields `_productDto` and
    `_serviceProvider`, a constructor, and a public async method `Map`
    to handle the mapping process. Necessary `using` directives for
    dependency injection and DTOs were also added.

---
+++ nopCommerce/AttributeSpecificationMapper.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using nopCommerceWebApiClient.Objects.Customer;
+using nopCommerceWebApiClient.Objects.Product;
+using nopCommerceWebApiClient.Objects.ProductSpecificationAttributeMapping;
+using nopCommerceWebApiClient.Objects.SpecificationAttribute;
+using nopCommerceWebApiClient.Objects.SpecyficationAttribute;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.nopCommerce
+{
+    /// <summary>
+    /// Class for mapping attribute specification to product.
+    /// 
+    /// Uses the AttributeSpecificationNopCommerce, AttributeSpecificationOptionNopCommerce, ProductSpecificationAttributeMappingNopCommerce attributes.
+    /// The minimal data class creates a new specification attribute mapping to the product with all needed dependent classes.
+    /// </summary>
+    public class AttributeSpecificationMapper
+    {
+        private readonly ProductDto _productDto;
+        private readonly IServiceProvider _serviceProvider;
+
+        public AttributeSpecificationMapper(ProductDto productDto, IServiceProvider serviceProvider)
+        {
+            _productDto = productDto;
+            _serviceProvider = serviceProvider;
+        }
+
+        /// <summary>
+        /// Map attribute specification to product.
+        /// </summary>
+        /// <param name="groupName">Product, Accessories, Board etc.</param>        
+        /// <param name="value">Color, Opening angle, etc.</param>
+        /// <param name="optionName">Red, Black, Left corner etc.</param>
+        public async Task Map(string groupName, string value, string optionName)
+        {
+            var attributeSpecificationService = _serviceProvider.GetService<AttributeSpecificationNopCommerce>();
+            SpecificationAttributeDto attributeSpecificationDto = await attributeSpecificationService.CreateSetAsync(groupName, value, optionName);
+
+            var attributeSpecificationOptionService = _serviceProvider.GetService<AttributeSpecificationOptionNopCommerce>();
+            var specificationAttributeOptionDto = await attributeSpecificationOptionService.GetBySpecificationAttributeIdAsync(attributeSpecificationDto.Id) ??
+                throw new CusomtException("SpecificationAttributeOption with specification attribute Id not exists");
+
+            await createAsync(specificationAttributeOptionDto);
+        }
+
+       
+    }
+}
