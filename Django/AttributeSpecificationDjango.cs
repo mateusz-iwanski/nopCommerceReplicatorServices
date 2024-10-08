db62f214550525063e0aa2e1fa546f514193803d
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-08 15:33:01 +0200

    Add Get method to AttributeSpecificationDjango class
    Added a new method `Get` to the `AttributeSpecificationDjango` class within the `nopCommerceReplicatorServices.Django` namespace. The `Get` method retrieves attribute specifications by product ID, constructs a SQL query, executes it, and maps the results to `AttributeSpecificationMapperDto` objects. It returns a list of attribute specifications or null if none are found.

--- Django/AttributeSpecificationDjango.cs
+++ Django/AttributeSpecificationDjango.cs
@@ -24,6 +24,47 @@ namespace nopCommerceReplicatorServices.Django
             return;
         }
 
-        
+        /// <summary>
+        /// Get attribute by product ID
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns>List of AttributeSpecificationMapperDto</returns>
+        public IEnumerable<AttributeSpecificationMapperDto>? Get(int productId)
+        {
+
+            var attributeSpecfificationList = new List<AttributeSpecificationMapperDto>();
+
+            var query = $@"
+                    SELECT 
+                        catalogue_product.id,
+                        attr.name as ""value"",
+                        attrgr.name
+                    FROM public.catalogue_product
+                    FULL JOIN catalogue_productattributevalue as attrval on attrval.product_id = catalogue_product.id
+                    FULL JOIN catalogue_productattribute as attr on attrval.attribute_id = attr.id
+                    FULL JOIN catalogue_attributeoptiongroup as attrgr on attrgr.id = attr.option_group_id
+                    WHERE value_boolean = true and catalogue_product.id = {productId};
+            ";
+
+            dbConnector.OpenConnection();
+
+            dbConnector.ExecuteQuery(query, (reader) =>
+            {
+                while (reader.Read())
+                {
+                    var attributeSpecfification = new AttributeSpecificationMapperDto(
+                        "Product", 
+                        reader.GetString(reader.GetOrdinal("value")), 
+                        reader.GetString(reader.GetOrdinal("name"))
+                        );
+
+                    attributeSpecfificationList.Add(attributeSpecfification);
+                }
+            });
+
+            dbConnector.CloseConnection();
+
+            return attributeSpecfificationList.Count > 0 ? attributeSpecfificationList : null;
+        }
     }
 }
