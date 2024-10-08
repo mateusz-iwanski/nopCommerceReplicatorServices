d1dff549e2e0cdc0084e5483bf9d91d14185a53a
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-08 15:32:47 +0200

    Add AttributeSpecificationDjango class for Django attributes
    Introduce AttributeSpecificationDjango class in the
    nopCommerceReplicatorServices.Django namespace to handle
    attribute specifications from Django source data. Import
    necessary namespaces including Newtonsoft.Json.Linq,
    nopCommerceReplicatorServices.nopCommerce,
    nopCommerceWebApiClient.Objects.Customer, and various System
    namespaces. Initialize dbConnector in the constructor with
    "Django" and "postgresql" parameters and call Initialize method.

---
+++ Django/AttributeSpecificationDjango.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json.Linq;
+using nopCommerceReplicatorServices.nopCommerce;
+using nopCommerceWebApiClient.Objects.Customer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Django
+{
+    /// <summary>
+    /// Attribute from Django source data
+    /// </summary>
+    public class AttributeSpecificationDjango
+    {
+        private DBConnector dbConnector { get; set; }
+
+        public AttributeSpecificationDjango()
+        {
+            dbConnector = new DBConnector("Django", "postgresql");
+            dbConnector.Initialize();
+            return;
+        }
+
+        
+    }
+}
