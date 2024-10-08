468619fdacae66d049aaf326602c6fd84d83f2da
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-08 15:36:08 +0200

    Refactor attribute specification handling
    Removed AttributeSpecificationDjango class and its methods from AttributeSpecificationDjango.cs. Added IAttributeSpecificationSourceData interface with Get method in IAttributeSpecificationSourceData.cs.

---
+++ Django/IAttributeSpecificationSourceData.cs
@@ -0,0 +1,9 @@
+﻿using nopCommerceReplicatorServices.nopCommerce;
+
+namespace nopCommerceReplicatorServices.Django
+{
+    public interface IAttributeSpecificationSourceData
+    {
+        IEnumerable<AttributeSpecificationMapperDto>? Get(int productId);
+    }
+}
