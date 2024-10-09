1ab7d53daf1a583469e867e78cea8f1243b75478
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-09 15:50:44 +0200

    Add CommandArguments class for handling CLI parameters
    Introduced a new file `CommandArguments.cs` in the `nopCommerceReplicatorServices` namespace. This file defines the `CommandArguments` class, which encapsulates various command-line arguments, including customer IDs, help flag, show details option, service to replicate, and several product ID options. This class provides a structured way to manage and access these parameters.

---
+++ CommandArguments.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices
+{
+    /// <summary>
+    /// Arguments for Command Line
+    /// </summary>
+    public class CommandArguments
+    {
+        public int RepCustomerId { get; set; }
+        public int ShCustomerId { get; set; }
+        public bool Help { get; set; }
+        public bool ShowDetailsOption { get; set; }
+        public string ServiceToReplicate { get; set; }
+        public int ShProductIdOption { get; set; }
+        public int RepProductIdOption { get; set; }
+        public int RepInventoryProductIdOption { get; set; }
+        public int RepAttributeSpecificationProductIdOption { get; set; }
+    }   
+}
