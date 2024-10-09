20933e53d0637d46922e92c653ffbae29f4235d1
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-09 15:30:00 +0200

    Add IProductSpecificationAttributeMapping interface
    Introduced a new namespace `nopCommerceReplicatorServices.Actions` and
    added the `IProductSpecificationAttributeMapping` interface. This
    interface includes a read-only property `ServiceKeyName`, and two
    asynchronous methods: `CreateAsync` and `GetByIdsAsync`. Also added a
    new `using` directive for `nopCommerceWebApiClient.Objects.ProductSpecificationAttributeMapping`.

---
+++ Actions/IProductSpecificationAttributeMapping.cs
@@ -0,0 +1,11 @@
+﻿using nopCommerceWebApiClient.Objects.ProductSpecificationAttributeMapping;
+
+namespace nopCommerceReplicatorServices.Actions
+{
+    public interface IProductSpecificationAttributeMapping
+    {
+        string ServiceKeyName { get; }
+        Task<List<HttpResponseMessage>> CreateAsync(int productId, IAttributeSpecificationSourceData attributeSpecExternal, Service setService);
+        Task<ProductSpecificationAttributeMappingDto>? GetByIdsAsync(int productId, int attributeSpecificationOptionId);
+    }
+}
