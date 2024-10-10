39e774dcd32d9525830920452578a34566ac2306
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-10 13:58:08 +0200

    Refactor: Remove setService param from CreateAsync
    The method signature of `CreateAsync` in the `IProductSpecificationAttributeMapping` interface has been modified. The `Service setService` parameter has been removed from the method.

--- Actions/IProductSpecificationAttributeMapping.cs
+++ Actions/IProductSpecificationAttributeMapping.cs
@@ -5,7 +5,7 @@ namespace nopCommerceReplicatorServices.Actions
     public interface IProductSpecificationAttributeMapping
     {
         string ServiceKeyName { get; }
-        Task<List<HttpResponseMessage>> CreateAsync(int productId, IAttributeSpecificationSourceData attributeSpecExternal, Service setService);
+        Task<List<HttpResponseMessage>> CreateAsync(int productId, IAttributeSpecificationSourceData attributeSpecExternal);
         Task<ProductSpecificationAttributeMappingDto>? GetByIdsAsync(int productId, int attributeSpecificationOptionId);
     }
 }
