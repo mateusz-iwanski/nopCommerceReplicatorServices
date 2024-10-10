2ab29f3191441e140f270e0ea3555f76420fa1a7
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-10 13:58:50 +0200

    Update Get method to use SubiektGT service ID
    Updated the XML documentation for the `productId` parameter in the `Get` method to specify it refers to the "SubiektGT service ID". Modified the SQL query within the `Get` method to filter based on `subiekt_gt_id` instead of `catalogue_product.id`.

--- Django/AttributeSpecificationDjango.cs
+++ Django/AttributeSpecificationDjango.cs
@@ -28,7 +28,7 @@ namespace nopCommerceReplicatorServices.Django
         /// <summary>
         /// Get attribute by product ID
         /// </summary>
-        /// <param name="productId"></param>
+        /// <param name="productId">SubiektGT service ID</param>
         /// <returns>List of AttributeSpecificationMapperDto, null if not exists</returns>
         public IEnumerable<AttributeSpecificationMapperDto>? Get(int productId)
         {
@@ -44,7 +44,7 @@ namespace nopCommerceReplicatorServices.Django
                     FULL JOIN catalogue_productattributevalue as attrval on attrval.product_id = catalogue_product.id
                     FULL JOIN catalogue_productattribute as attr on attrval.attribute_id = attr.id
                     FULL JOIN catalogue_attributeoptiongroup as attrgr on attrgr.id = attr.option_group_id
-                    WHERE value_boolean = true and catalogue_product.id = {productId};
+                    WHERE value_boolean = true and subiekt_gt_id = {productId};
             ";
 
             dbConnector.OpenConnection();
