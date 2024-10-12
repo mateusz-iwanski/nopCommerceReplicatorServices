cae6642033b9df7d7fb8e596acd0f1cb3cd2c6eb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-12 18:48:06 +0200

    Add UnreplicatedDataException for data replication issues
    Introduced a new exception class `UnreplicatedDataException` in the
    `nopCommerceReplicatorServices.Exceptions` namespace. This class
    inherits from `CustomException` and is intended to be thrown when
    data has not been replicated yet. The exception message provides
    guidance on checking the DataBinding database and manually adding
    data if necessary. This addition aids in managing and debugging
    data replication issues in the nopCommerce replication services.

---
+++ Exceptions/UnreplicatedDataException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Exceptions
+{
+    /// <summary>
+    /// Thrown the exception when the data hasn't been replicated yet. 
+    /// It is used to prevent data replication when 
+    /// for example you want to update object which hasn't been replicated yet.
+    /// </summary>
+    public class UnreplicatedDataException : CustomException
+    {
+        public UnreplicatedDataException(string? message) : base(
+            $"{message} Check the DataBinding database. If you are deleting or adding a new object " +
+            $"via nopCommerce and not via replicator, you need to manually add the data to the DataBinding database " +
+            $"for correct replication.")
+        {
+        }
+    }
+}
