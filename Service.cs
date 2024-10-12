4705f900d938b98bf239a5c04c0ad47cf4fe1468
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-12 19:02:15 +0200

    Enhance Service enum and add LINQ directive
    Added a `using System.Linq;` directive to the `Service.cs` file to enable LINQ functionalities. Introduced a summary documentation comment for the `Service` enum to explain its purpose and usage. Explicitly assigned the value `0` to the `SubiektGT` member of the `Service` enum, indicating it as the primary service for creating and linking products/clients with nopCommerce.

--- Service.cs
+++ Service.cs
@@ -6,9 +6,15 @@ using System.Threading.Tasks;
 
 namespace nopCommerceReplicatorServices
 {
+    /// <summary>
+    /// Represents the service to replicate data from.
+    /// 
+    /// In Service value 0 has the main service which is used to create and link products/clients etc. 
+    /// with nopCommerce. Link data you can find in DataBinding database.
+    /// </summary>
     public enum Service
     {
-        SubiektGT,
+        SubiektGT = 0,
         Django,
     }
 }
