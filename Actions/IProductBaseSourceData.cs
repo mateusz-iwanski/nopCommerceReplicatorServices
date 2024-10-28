2ee4e15274427498555ab9e19abfe63d3ca88a74
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-28 20:09:45 +0100

    Add IProductBaseSourceData interface for product actions
    Introduce IProductBaseSourceData interface in nopCommerceReplicatorServices.Actions namespace to handle product actions for Subiekt GT. Includes async methods for retrieving product data, inventory, and prices.

---
+++ Actions/IProductBaseSourceData.cs
@@ -0,0 +1,20 @@
+﻿using nopCommerceWebApiClient.Objects.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.Actions
+{
+    /// <summary>
+    /// This interface is used to implement product actions for external vase service (Subiekt GT) with source data (e.g. ProductGt).
+    /// </summary>
+    public interface IProductBaseSourceData
+    {
+        Task<IEnumerable<ProductCreateMinimalDto>>? GetAsync(string fieldName, object fieldValue);
+        Task<ProductCreateMinimalDto>? GetByIdAsync(int customerId);
+        Task<ProductUpdateBlockInventoryDto>? GetInventoryByIdAsync(int customerId);
+        Task<ProductUpdateBlockPriceDto>? GetProductPriceByIdAsync(int productId);
+    }
+}
