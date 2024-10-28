8e8032c0976d6e62c63dd7130c33fb671cd7805f
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-28 20:11:20 +0100

    Add WarehouseCode enum in GtvFirebase namespace
    Introduce a new namespace `nopCommerceReplicatorServices.GtvFirebase`
    and within it, define the `WarehouseCode` enum representing two
    warehouses: `M_MLP` (default) and `M_PROFIL` (long elements).
    Include necessary `using` directives for system namespaces.

---
+++ GtvFirebase/WarehouseCode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.GtvFirebase
+{
+    /// <summary>
+    /// GTV has two warehouses. This enum represents them.
+    /// </summary>
+    public enum WarehouseCode
+    {
+        M_MLP,  // default warehouse
+        M_PROFIL  // warehouse with long elements
+    }
+}
