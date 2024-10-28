9982ed8d9661b076a528e745f0eb95daf77d6187
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-28 20:12:58 +0100

    Add ObjectToBind enum for data binding in new file
    Introduce ObjectToBind.cs with using directives and a new namespace `nopCommerceReplicatorServices`. Define an enum `ObjectToBind` with members `Product` and `Customer` for selecting DataBinding objects.

---
+++ ObjectToBind.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices
+{
+    /// <summary>
+    /// Used to select a DataBinding object for binding.
+    /// </summary>
+    public enum ObjectToBind
+    {
+        Product,
+        Customer
+    }
+}
