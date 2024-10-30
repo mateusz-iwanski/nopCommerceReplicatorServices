4cf19d428200e639831543d78aa3bb6fea9ae7ad
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-30 17:46:14 +0100

    Implement IDisposable and update DataBinding methods
    The DataBinding class now implements the IDisposable interface.
    Added Google.Api.Gax.Grpc and System.Net.Security namespaces.
    Introduced a private readonly field _dbContext of type KeyBindingDbContext.
    Modified the constructor to initialize _dbContext using the service provider.
    Added a method GetDbContext to return the _dbContext instance.
    Updated the BindKey method to accept Service, ObjectToBind, and int externalId parameters, and save a DataBindingEntity to the database.
    Added a new method GetKeyBindingByExternalId to retrieve a DataBindingEntity by Service, ObjectToBind, and int externalId.
    Renamed and updated the GetKeyBinding method to GetKeyBindingByNopCommerceId, which now retrieves a DataBindingEntity by Service, ObjectToBind, and int nopCommerceId.
    Implemented the Dispose method to dispose of the _dbContext.

--- DataBinding/DataBinding.cs
+++ DataBinding/DataBinding.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Google.Api.Gax.Grpc;
+using Microsoft.Extensions.DependencyInjection;
+using nopCommerceReplicatorServices.nopCommerce;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Security;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,60 +19,80 @@ namespace nopCommerceReplicatorServices.DataBinding
     /// so that we know which nopCommerce customer is which customer on the external site or 
     /// whether the client has already been replicated from an external service.
     /// </remarks>
-    public class DataBinding
+    public class DataBinding : IDisposable
     {
-        IServiceProvider _serviceProvider;
+        private readonly IServiceProvider _serviceProvider;
+        private readonly KeyBindingDbContext _dbContext;
 
         public DataBinding(IServiceProvider serviceProvider)
         {
             _serviceProvider = serviceProvider;
+            _dbContext = _serviceProvider.CreateScope().ServiceProvider.GetRequiredService<KeyBindingDbContext>();
+        }
+
+        public KeyBindingDbContext GetDbContext()
+        {
+            return _dbContext;
         }
 
         /// <summary>
         /// Bind a ID from nopCommerce with a serviceName (ID, symbol etc.) from an external service.
         /// </summary>
         /// <param name="nopCommerceId">The ID from nopCommerce</param>
-        /// <param name="serviceName">The key name of the external service (for example: should be the same as ICustomer.ServiceKeyName)</param>  
-        /// <param name="serviceKey">The key name of the external service. Identifier, symbol... any unique you want to use)</param>
-        /// <param name="serviceValue">The value of the external service. For example - if ID - "1","2","3" ... If symbol - "u1" "sas3" ... </param>
-        public void BindKey(int nopCommerceId, string serviceName, string serviceKey, string serviceValue)
+        /// <param name="service">The key name of the external service. For example Django, GtvApi etc.</param>  
+        /// <param name="objectToBind">The name of object we want to bind. For example Customer, Product etc.</param>
+        /// <param name="externalId">The value of the external service. For example - if ID - "1","2","3" ... If symbol - "u1" "sas3" ... </param>
+        public void BindKey(int nopCommerceId, Service service, ObjectToBind objectToBind, int externalId)
         {
-            using (var scope = _serviceProvider.CreateScope())
+            var customerKeyBinding = new DataBindingEntity
             {
-                var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
+                NopCommerceId = nopCommerceId,
+                Service = service,
+                BindedObject = objectToBind,
+                ExternalId = externalId
+            };
 
-                var customerKeyBinding = new DataBindingEntity
-                {
-                    NopCommerceId = nopCommerceId,
-                    ServiceName = serviceName,
-                    ServiceKey = serviceKey,
-                    ServiceValue = serviceValue
-                };
-
-                dbContext.DataBinding.Add(customerKeyBinding);
-                dbContext.SaveChanges();
-            }
-        }
+            _dbContext.DataBinding.Add(customerKeyBinding);
+            _dbContext.SaveChanges();
+        }       
 
         /// <summary>
-        /// Get client binding information.
-        /// If it isn't found, we know it hasn't been replicated yet.
+        /// Get binding object by external service ID.
         /// </summary>
         /// <param name="serviceName">The key name of the external service (should be the same as ICustomer.ServiceKeyName)</param>  
-        /// <param name="serviceKey">The key name of the external service. Identifier, symbol... any unique you used)</param>
-        /// <param name="serviceValue">The value of the external service. For example - if used ID - "1","2","3" ... If used symbol - "u1" "sas3" ... </param>
-        public DataBindingEntity? GetKeyBinding(Service serviceName, string serviceKey, string serviceValue)
+        /// <param name="bindedObject">The object which want to find Product, Customer, etc.</param>
+        /// <param name="externalId">The ID of the external service</param>
+        public DataBindingEntity? GetKeyBindingByExternalId(Service serviceName, ObjectToBind bindedObject, int externalId)
         {
             using (var scope = _serviceProvider.CreateScope())
             {
                 var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
 
                 return dbContext.DataBinding.FirstOrDefault(x =>
-                    x.ServiceName == serviceName.ToString() &&
-                    x.ServiceKey == serviceKey &&
-                    x.ServiceValue == serviceValue
+                    x.Service == serviceName &&
+                    x.BindedObject == bindedObject &&
+                    x.ExternalId == externalId
                 );
             }
+        }
+
+        public DataBindingEntity? GetKeyBindingByNopCommerceId(Service serviceName, ObjectToBind bindedObject, int nopCommerceId)
+        {
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetRequiredService<KeyBindingDbContext>();
+
+                return dbContext.DataBinding.FirstOrDefault(x =>
+                    x.Service == serviceName &&
+                    x.BindedObject == bindedObject &&
+                    x.NopCommerceId == nopCommerceId
+                );
+            }
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
         }
     }
 }
