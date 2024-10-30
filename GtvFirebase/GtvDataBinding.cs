a95ea78cff7a7ea498918ef6355bc247a600ffd8
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-30 10:10:46 +0100

    Add GtvDataBinding class for Subiekt GT and GTV integration
    Introduce GtvDataBinding class in nopCommerceReplicatorServices.GtvFirebase namespace to handle data binding between Subiekt GT and GTV products.
    
    - Add private readonly field `_dataBinding` of type `DataBinding.DataBinding`.
    - Implement `GetGtvIdBySubiekt(int subiektGtProductId)` to retrieve GTV product ID using Subiekt GT product ID, throwing `CustomException` if mappings are not found.
    - Implement `GetGtvIdByNopCommerce(int nopCommerceProductId)` to retrieve GTV product ID using nopCommerce product ID, throwing `CustomException` if mapping is not found.
    - Include necessary `using` directives for exceptions, Subiekt GT services, and other system collections and tasks.

---
+++ GtvFirebase/GtvDataBinding.cs
@@ -0,0 +1,51 @@
+﻿using nopCommerceReplicatorServices.Exceptions;
+using nopCommerceReplicatorServices.SubiektGT;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.GtvFirebase
+{
+    internal class GtvDataBinding
+    {
+        private readonly DataBinding.DataBinding _dataBinding;
+
+        public GtvDataBinding(DataBinding.DataBinding dataBinding)
+        {
+            _dataBinding = dataBinding;
+            return;
+        }
+
+        /// <summary>
+        /// Return GTV product id by Subiekt GT id.
+        /// 
+        /// First it finds nopCommerce id by Subiekt GT id, and next by nopCommerce id it finds GTV id.
+        /// </summary>
+        /// <param name="subiektGtProductId">ID from subiekt gt product</param>
+        /// <returns></returns>
+        /// <exception cref="CustomException">If not find subiekt gt product, if not find Gtv product id</exception>
+        public int GetGtvIdBySubiekt(int subiektGtProductId)
+        {
+            // find nopCommerce id by Subiekt GT id
+            var bindingDataNopCommerce = _dataBinding.GetKeyBindingByExternalId(Service.SubiektGT, ObjectToBind.Product, subiektGtProductId) ??
+                throw new CustomException($"Can't find product by Id - '{subiektGtProductId}' in DataBinding for service {Service.SubiektGT.ToString()}. You have to map it.");
+
+            // find GTV product id by nopCommerce id
+            var bindingDataGtv = _dataBinding.GetKeyBindingByNopCommerceId(Service.GtvApi, ObjectToBind.Product, bindingDataNopCommerce.NopCommerceId) ??
+                throw new CustomException($"Can't find product by Id - '{bindingDataNopCommerce.NopCommerceId}' in DataBinding for service {Service.GtvApi.ToString()}. You have to map it.");
+
+            return bindingDataGtv.ExternalId;
+        }
+
+        public int GetGtvIdByNopCommerce(int nopCommerceProductId)
+        {
+            var bindingDataNopCommerce = _dataBinding.GetKeyBindingByExternalId(Service.SubiektGT, ObjectToBind.Product, nopCommerceProductId) ??
+                throw new CustomException($"Can't find product by Id - '{nopCommerceProductId}' in DataBinding for service {Service.GtvApi.ToString()}. You have to map it.");
+
+            return bindingDataNopCommerce.ExternalId;
+        }
+
+    }
+}
