15f424e10a01f73c664433b950b61f695cf314cb
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-30 10:10:18 +0100

    Add AttributeType enum in GtvFirebase namespace
    Introduce a new namespace `nopCommerceReplicatorServices.GtvFirebase` and add an `AttributeType` enum with members: `Property`, `Description`, `AdditionalDescription`, `Photo360Url`, `YtUrl`, and `File`. Include necessary `using` directives.

---
+++ GtvFirebase/AttributeType.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.GtvFirebase
+{
+    /// <summary>
+    /// Represents item attribute type.
+    /// </summary>
+    public enum AttributeType
+    {
+        Property,
+        Description,
+        AdditionalDescription,
+        Photo360Url,
+        YtUrl,
+        File  
+    }
+}
