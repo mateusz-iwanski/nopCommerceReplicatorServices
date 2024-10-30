11e250aeb59d512094d6eabbac46cca85f549bac
Mateusz Mateusz <mateusz.iw@gmail.com>
2024-10-30 17:47:50 +0100

    Make KeyBindingDbContext public and add LINQ support
    The `System.Linq` namespace was added to the `KeyBindingDbContext.cs` file, indicating that LINQ functionalities are now being utilized. Additionally, the `KeyBindingDbContext` class was changed from `internal` to `public`, increasing its accessibility for use in other assemblies.

--- DataBinding/KeyBindingDbContext.cs
+++ DataBinding/KeyBindingDbContext.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace nopCommerceReplicatorServices.DataBinding
 {
-    internal class KeyBindingDbContext : DbContext
+    public class KeyBindingDbContext : DbContext
     {
         private IServiceProvider _serviceProvider { get; set; }
 
