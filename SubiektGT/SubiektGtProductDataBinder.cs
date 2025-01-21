1727341a31f3152ed88093b870e2b46f1a5da9fb
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-21 23:06:03 +0100

    Add SubiektGtProductDataBinder class for product binding
    Introduce SubiektGtProductDataBinder class in the
    nopCommerceReplicatorServices.SubiektGT namespace to facilitate
    binding of Subiekt GT products with nopCommerce products in the
    BindingData database. This class inherits from ProductDataBinderBase
    and implements the IProductDataBinder interface.
    
    - Add private readonly field _serviceProvider to store the service
      provider instance.
    - Initialize base class and _serviceProvider in the constructor.
    - Override BindAsync method to link Subiekt GT and nopCommerce
      products asynchronously.
    - Include necessary using directives for required namespaces.

---
+++ SubiektGT/SubiektGtProductDataBinder.cs
@@ -0,0 +1,39 @@
+﻿using nopCommerceReplicatorServices.DataBinding;
+using nopCommerceReplicatorServices.nopCommerce;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nopCommerceReplicatorServices.SubiektGT
+{
+    /// <summary>
+    /// Helpful class to bind Subiekt GT product with nopCommerce product in BindingData database.
+    /// </summary>
+    public class SubiektGtProductDataBinder : ProductDataBinderBase, IProductDataBinder
+    {
+        private readonly IServiceProvider _serviceProvider;
+
+        public SubiektGtProductDataBinder(DataBinding.DataBinding dataBinding, IServiceProvider service)
+           : base(dataBinding, Service.SubiektGT)
+        {
+            _serviceProvider = service;
+            return;
+        }
+
+        /// <summary>
+        /// Link in BindData database Subiekt GT product with nopCommerce product.
+        /// 
+        /// Before link a nopCommerce product with Subiekt GT product, product has to exist in nopCommerce 
+        /// and nopCommerce product has to be linked with SubiektGT service.
+        /// </summary>
+        /// <param name="nopCommerceProductId"></param>
+        /// <param name="gtvId"></param>
+        /// <exception cref="Exceptions.CustomException"></exception>
+        public override async Task BindAsync(int nopCommerceProductId, int gtvId)
+        {
+            await BindProductAsync(_serviceProvider, nopCommerceProductId, gtvId);
+        }
+    }
+}
