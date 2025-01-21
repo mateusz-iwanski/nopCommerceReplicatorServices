6616d45ba1a1b6616e93971cd5526166f0363296
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-21 23:05:32 +0100

    Add INoSqlDbService interface for Azure Cosmos DB operations
    Introduce INoSqlDbService interface in nopCommerceReplicatorServices.NoSQLDB
    namespace. This interface defines async methods for Cosmos DB operations,
    including checking existence of databases/containers, CRUD operations on
    items, and querying items. Ensures consistent interaction with Cosmos DB.

---
+++ NoSQLDB/INoSqlDbService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.Cosmos;
+
+namespace nopCommerceReplicatorServices.NoSQLDB
+{
+    public interface INoSqlDbService
+    {
+        Task<bool> DatabaseExistsAsync(string databaseName);
+        Task<bool> ContainerExistsAsync(string databaseName, string containerName);
+        Task<ItemResponse<T>> CreateItemAsync<T>(T cosmosDto) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> GetItemAsync<T>(string id, string containerName, string uri) where T : CosmosDbDtoBase;
+        Task<List<T>> GetByQueryAsync<T>(QueryDefinition query, string containerName) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> UpdateItemAsync<T>(T item) where T : CosmosDbDtoBase;
+        Task<ItemResponse<T>> DeleteItemAsync<T>(string itemId, string containerName, string partitionKeyData) where T : CosmosDbDtoBase;
+        Task CreateDatabaseAndContainerIfNotExistsAsync(string databaseName, string containerName, string partitionKeyPath);
+    }
+}
