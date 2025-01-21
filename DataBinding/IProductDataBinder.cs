706183804be8d341332770722670cfc6091f7824
Mateusz Mateusz <mateusz.iw@gmail.com>
2025-01-21 22:18:23 +0100

    Add IProductDataBinder interface with BindAsync method
    Introduced a new namespace `nopCommerceReplicatorServices.DataBinding` and added the `IProductDataBinder` interface. This interface includes the `BindAsync` method for binding product data between nopCommerce and another system identified by `gtvId`. Commented out methods related to stock and price replication: `RemoveStockReplication`, `SetPriceReplicationAsync`, `RemovePriceReplication`, and `SetStockReplication`.

---
+++ DataBinding/IProductDataBinder.cs
@@ -0,0 +1,11 @@
+﻿namespace nopCommerceReplicatorServices.DataBinding
+{
+    public interface IProductDataBinder
+    {
+        Task BindAsync(int nopCommerceProductId, int gtvId);
+        //void RemoveStockReplication(int productNopCommerceId);
+        //void SetPriceReplicationAsync(int externalProductId);
+        //void RemovePriceReplication(int externalProductId);
+        //void SetStockReplication(int productNopCommerceId);
+    }
+}
